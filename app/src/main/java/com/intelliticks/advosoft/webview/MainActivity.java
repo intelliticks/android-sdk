b8a1b52e5941dd3078deda66ee4e4cb2ecbb55e1
RakeshDey <rakesh.advocosoft@gmail.com>
2018-11-02 17:44:57 +0530

    “version1”

---
+++ app/src/main/java/com/intelliticks/advosoft/webview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.intelliticks.advosoft.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.intelliticks.webview.IntelliTicksWebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       /* new IntelliTicksWebView.Builder(this).titleDefault("The Finest Artist").webViewJavaScriptEnabled(true)
+                .show("https://app.intelliticks.com/landing/pSZJr5kbAY7jEwEtf_c/pricing");*/
+        ////                    .toolbarScrollFlags(0)
+               ///             .webViewJavaScriptEnabled(true)
+        //                    .webViewUseWideViewPort(false)
+        ////                    .show("http://andrewliu.in/2016/01/30/聊聊Redis的订阅发布/");
+        //                    .show("http://www.youtube.com");
+
+
+        new IntelliTicksWebView.Builder(this)
+                .initilizeKey("pSZJr5kbAY7jEwEtf_c")
+                .action("pricing")
+                 .loadWeb();
+
+    }
+}
